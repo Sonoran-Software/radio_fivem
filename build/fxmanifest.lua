ec9e82634509b725da373bc602a2ca61046b1698
Lansing <me@lansing.io>
2021-09-29 22:59:48 -0400

    Prod fxmanifest.lua

---
+++ build/fxmanifest.lua
@@ -0,0 +1,12 @@
+-- required fxmanifest stuff
+fx_version 'cerulean'
+game 'gta5'
+
+-- setup for nui
+files {
+    'html/**/*',
+    'static/**/*'
+} 
+ui_page 'html/ui.html'
+
+client_script 'cl.lua'
