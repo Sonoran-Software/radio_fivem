0e9e6a1dd748a6fdeff5031f2b73fc703c6e572c
Lansing <me@lansing.io>
2021-10-04 16:06:30 -0400

    Release/1.0.3

--- build/fxmanifest.lua
+++ build/fxmanifest.lua
@@ -4,7 +4,7 @@ game 'gta5'
 
 author 'Sonoran Software Systems LLC'
 description 'Sonoran Radio FiveM Integration'
-version '1.0.2'
+version '1.0.3'
 
 server_scripts {
     'config.lua',
--- fxmanifest.lua
+++ fxmanifest.lua
@@ -11,7 +11,7 @@ game 'gta5'
 
 author 'Sonoran Software Systems LLC'
 description 'Sonoran Radio FiveM Integration'
-version '1.0.2'
+version '1.0.3'
 
 server_scripts {
     'dist/test/config.lua',
