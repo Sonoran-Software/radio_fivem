c0e236a245cff50754a7402ab5eb7af0190de602
Lansing <me@lansing.io>
2021-11-06 00:52:34 -0400

    Corrected Plus/Pro

--- src/components/Channels.vue
+++ src/components/Channels.vue
@@ -14,7 +14,7 @@
         <div class="sc-body" v-if="$store.state.sublvl == 0">
             <div class="sc-ch">
                 <div class="sc-ch-header">View Only</div>
-                <div class="sc-ch-freq">Upgrade to Pro</div>
+                <div class="sc-ch-freq">Upgrade to Plus</div>
             </div>
             <div class="sc-ch" v-for="preset in $store.state.presets" :key="preset.display_name">
                 <div class="sc-ch-header">{{ preset.display_name }}</div>
