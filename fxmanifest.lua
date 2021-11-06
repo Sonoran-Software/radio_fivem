07254c1c956f1ec87fd3b9ab206a195fdb3e856c
Sithis <zeeflop@gmail.com>
2021-11-06 14:26:45 -0500

    add towers to stream and update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,12 +6,15 @@
 fx_version 'cerulean'
 game 'gta5'
 
+data_file 'DLC_ITYP_REQUEST' 'stream/prop_radio_tower.ytyp'
+
 shared_script 'config.lua'
 server_script 'lua/**/sv_*.lua'
 client_script 'lua/**/cl_*.lua'
 
 -- setup for nui
 files {
+    'stream/*',
     'dist/**/*',
     'static/**/*'
 } 
