4174a58a10231d43f2780b718c8210bce5d99e2b
Jacob Parker <blocckba5her@gmail.com>
2021-12-20 23:02:31 -0800

    tweak(stream): move stream to separate dev resource

--- build/sonoranradio/fxmanifest.lua
+++ build/sonoranradio/fxmanifest.lua
@@ -24,3 +24,12 @@ files {
     'static/**/*'
 } 
 ui_page 'dist/ui.html'
+
+-- setup for streamed files
+files {
+    'data/vehicles.meta',
+    'data/carvariations.meta',
+}
+data_file 'VEHICLE_METADATA_FILE' 'data/vehicles.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/carvariations.meta'
+data_file 'DLC_ITYP_REQUEST' 'stream/prop_radio_tower.ytyp'
---
+++ sonrad_tower/fxmanifest.lua
@@ -0,0 +1,17 @@
+-- !!THIS IS THE DEVELOPMENT MANIFEST!! --
+
+-- PRODUCTION MANIFEST IS LOCATED HERE; --
+-- ./build/sonoranradio/fxmanifest.lua	--
+
+fx_version 'cerulean'
+game 'gta5'
+
+files {
+	'data/vehicles.meta',
+	'data/carvariations.meta',
+}
+
+data_file 'VEHICLE_METADATA_FILE' 'data/vehicles.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/carvariations.meta'
+
+data_file 'DLC_ITYP_REQUEST' 'stream/prop_radio_tower.ytyp'
