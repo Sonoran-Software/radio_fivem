4d545dce77e6ca74b2daf836bf9eef9473741866
Jacob Parker <blocckba5her@gmail.com>
2022-01-19 15:14:11 -0800

    fix(towers): add range to spawn physical towers

--- lua/cl_towers.lua
+++ lua/cl_towers.lua
@@ -24,33 +24,19 @@ local function GetTowerCoords(tower)
         return tower.PropPosition
     end
 end
-local function GetClosestTower()
-    if #Towers < 1 then
-        DebugPrint("no towers spawned")
-        return nil, nil
-    end
-    local pedLocation = GetEntityCoords(GetPlayerPed(-1))
-    local d, dObj
-    for i = 1, #Towers do
-        local location = GetTowerCoords(Towers[i])
-        local dist = GetDistance(location, pedLocation)
-        if d == nil or dist < d then
-            d = dist
-            dObj = Towers[i]
-        end
-    end
-    return dObj, d
-end
 -- returns a value from 0-1 representing the percentage of active dishes
 local function GetTowerCapacity(tower)
-    if not tower.Dishes or #tower.Dishes < 1 then return 1.0 end
+    if #tower.DishStatus < 1 then
+        return 1.0
+    end
+
     local n = 0.0
-    for i = 1, #tower.Dishes do
-        if not IsEntityDead(tower.Dishes[i]) then
+    for i = 1, #tower.DishStatus do
+        if tower.DishStatus[i] == 'alive' then
             n = n + 1.0
         end
     end
-    return n / #tower.Dishes
+    return n / #tower.DishStatus
 end
 
 local function AddTowerRange(t)
@@ -61,50 +47,70 @@ local function AddTowerRange(t)
     SetBlipColour(blip, 3)
 end
 
-local function CreateTowerDishes(tower)
-    -- delete the other dishes if they exist
-    if tower.Dishes then
-        for _, e in ipairs(tower.Dishes) do
-            DeleteEntity(e)
-        end
-    end
-    tower.Dishes = {}
 
-    -- create dishes around tower base (in a circle)
+-- creates one dish on a tower
+local function CreateTowerDish(tower, index, n)
     local dishModel = GetHashKey("sonoran")
     LoadModelSync(dishModel)
-    local n = 4
-    for i = 0, n - 1 do
-        local theta = (i * math.pi * 2) / n
-        -- in a normal unit circle, cos is x and sin is y. however, we need y to be the forward direction (and 1 @ theta=0.0)
-        -- so we do some unconventional stuff here to ascertain the offsets
-        local offx = -math.sin(theta) * 1.6
-        local offy = math.cos(theta) * 1.6
 
-        local offset = GetOffsetFromEntityInWorldCoords(tower.Handle, offx, offy, 11.75)
-        local dishHndl = CreateVehicle(dishModel, offset.x, offset.y, offset.z, 0.0, false, false)
-        SetEntityAsMissionEntity(dishHndl, true, true)
-        FreezeEntityPosition(dishHndl, true)
-        if tower.Destruction then
-            SetVehicleStrong(dishHndl, true)
-        else
-            -- turning each dish invincible will make it impossible to destroy the dishes
-            -- in turn, disabling tower destruction
-            SetEntityInvincible(dishHndl, true)
-        end
+    local theta = ((index - 1) * math.pi * 2) / n
+    -- in a normal unit circle, cos is x and sin is y. however, we need y to be the forward direction (and 1 @ theta=0.0)
+    -- so we do some unconventional stuff here to ascertain the offsets
+    local offx = -math.sin(theta) * 1.6
+    local offy = math.cos(theta) * 1.6
 
-        -- set the decorator to "1" to alert lower functions that this is a dish
-        -- NOTE: later, this is set to 0 when the dish is killed. this is so that the dish doesn't get "destroyed" when it's killed
-        DecorSetInt(dishHndl, "sonrad_dish", 1)
-
-        -- update the rotation of the antenna based on the initial rotation of the tower + theta
-        -- also, thanks GTA for working in radians in some places and degrees in others (╯°□°）╯︵ ┻━┻
-        local baseRot = GetEntityRotation(tower.Handle, 0)
-        SetEntityRotation(dishHndl, baseRot.x, baseRot.y, baseRot.z + (theta * 180 / math.pi), 0, true)
-        table.insert(tower.Dishes, dishHndl)
+    local offset = GetOffsetFromEntityInWorldCoords(tower.Handle, offx, offy, 11.75)
+    local dishHndl = CreateVehicle(dishModel, offset.x, offset.y, offset.z, 0.0, false, false)
+    SetEntityAsMissionEntity(dishHndl, true, true)
+    FreezeEntityPosition(dishHndl, true)
+    if tower.Destruction then
+        SetVehicleStrong(dishHndl, true)
+    else
+        -- turning each dish invincible will make it impossible to destroy the dishes
+        -- in turn, disabling tower destruction
+        SetEntityInvincible(dishHndl, true)
     end
+
+    -- set the decorator to "1" to alert lower functions that this is a dish
+    -- NOTE: later, this is set to 0 when the dish is killed. this is so that the dish doesn't get "destroyed" when it's killed
+    DecorSetInt(dishHndl, "sonrad_dish", 1)
+
+    -- update the rotation of the antenna based on the initial rotation of the tower + theta
+    -- also, thanks GTA for working in radians in some places and degrees in others (╯°□°）╯︵ ┻━┻
+    local baseRot = GetEntityRotation(tower.Handle, 0)
+    SetEntityRotation(dishHndl, baseRot.x, baseRot.y, baseRot.z + (theta * 180 / math.pi), 0, true)
+
     SetModelAsNoLongerNeeded(dishModel)
+    if not tower.Dishes then tower.Dishes = {} end
+    if DoesEntityExist(tower.Dishes[index]) then
+        DeleteEntity(tower.Dishes[index])
+    end
+    tower.Dishes[index] = dishHndl
 end
+-- syncs the dead/alive state of dishes to their physical vehicles
+local function SyncDishStatus(tower, playSound)
+    for i = 1, #tower.Dishes do
+        local dish = tower.Dishes[i]
+        local dead = IsEntityDead(dish)
+
+        if tower.DishStatus[i] ~= 'alive' and not dead then
+            NetworkExplodeVehicle(dish, false, false)
+            DecorSetInt(dish, "sonrad_dish", 0)
+            -- play a power-down sound for the player
+            if playSound then
+                local coords = GetEntityCoords(dish)
+                PlaySoundFromCoord(-1, "Power_Down", coords, "DLC_HEIST_HACKING_SNAKE_SOUNDS", 0, 80)
+            end
+        elseif tower.DishStatus[i] == 'alive' and dead then
+            CreateTowerDish(tower, i, #tower.DishStatus)
+            if playSound then
+                local coords = GetEntityCoords(dish)
+                PlaySoundFromCoord(-1, "Success", coords, "DLC_HEIST_HACKING_SNAKE_SOUNDS", 0, 80)
+            end
+        end
+    end
+end
+
 local function CreateTowerLadder(tower)
     if tower.Ladder then
         DeleteEntity(tower.Ladder)
@@ -119,95 +125,95 @@ local function CreateTowerLadder(tower)
     tower.Ladder = ladder
     SetModelAsNoLongerNeeded(model)
 end
+-- fully creates a tower based on the given tower object
 local function CreateTower(tower)
-    if not Config.noPhysicalTowers then
-        local towerModel = GetHashKey("prop_radio_tower")
-        LoadModelSync(towerModel)
+    local towerModel = GetHashKey("prop_radio_tower")
+    LoadModelSync(towerModel)
 
-        local coords = tower.PropPosition
-        tower.Handle = CreateObject(towerModel, coords, false, false, false)
-        while not DoesEntityExist(tower.Handle) do Wait(0) end
-        FreezeEntityPosition(tower.Handle, true)
-        SetEntityCoords(tower.Handle, coords.x, coords.y, coords.z - 1, true, true, true, false)
-        PlaceObjectOnGroundProperly(tower.Handle)
+    local coords = tower.PropPosition
+    tower.Handle = CreateObject(towerModel, coords, false, false, false)
+    while not DoesEntityExist(tower.Handle) do Wait(0) end
+    FreezeEntityPosition(tower.Handle, true)
+    SetEntityCoords(tower.Handle, coords.x, coords.y, coords.z - 1, true, true, true, false)
+    PlaceObjectOnGroundProperly(tower.Handle)
 
-        SetModelAsNoLongerNeeded(towerModel)
-        CreateTowerDishes(tower)
-        CreateTowerLadder(tower)
+    SetModelAsNoLongerNeeded(towerModel)
+
+    -- spawn the dishes and ladder for the tower
+    for i = 1, #tower.DishStatus do
+        CreateTowerDish(tower, i, #tower.DishStatus)
     end
-    AddTowerRange(tower)
+    SyncDishStatus(tower, false)
+    CreateTowerLadder(tower)
+    tower.Spawned = true
+end
+-- delete the physical tower entities
+local function DestroyTower(tower)
+    if DoesEntityExist(tower.Handle) then
+        DeleteEntity(tower.Handle)
+    end
+    if DoesEntityExist(tower.Ladder) then
+        DeleteEntity(tower.Ladder)
+    end
+    local n = tower.Dishes and #tower.Dishes or 0
+    for j = 1, n do
+        DeleteEntity(tower.Dishes[j])
+    end
+    tower.Dishes = {}
+    tower.Spawned = false
 end
 
 RegisterNetEvent("RadioTower:SyncTowers")
 AddEventHandler("RadioTower:SyncTowers", function(towers)
+    -- make sure all towers are cleared before we sync
+    for i = 1, #Towers do DestroyTower(Towers[i]) end
+
     Towers = towers
-    DebugPrint(("synced %s"):format(json.encode(towers)))
-    if not HasSpawnedTowers then
-        for i = 1, #Towers do
-            CreateTower(Towers[i])
-        end
-        HasSpawnedTowers = true
+    for i = 1, #Towers do
+        AddTowerRange(Towers[i])
     end
-    DebugPrint("Synced towers")
+    DebugPrint(("synced %s"):format(json.encode(towers)))
 end)
 
 RegisterNetEvent("RadioTower:SpawnTower")
 AddEventHandler("RadioTower:SpawnTower", function(tower)
-    CreateTower(tower)
+    DebugPrint(("spawned %s"):format(json.encode(tower)))
     table.insert(Towers, tower)
-    if not HasSpawnedTowers then HasSpawnedTowers = true end
+    AddTowerRange(tower)
 end)
 
-RegisterNetEvent("RadioTower:KillDish")
-AddEventHandler("RadioTower:KillDish", function(towerId, dishIndex)
-    -- find the closest dish to the coordinates
+RegisterNetEvent('RadioTower:SetDishStatus')
+AddEventHandler('RadioTower:SetDishStatus', function(towerId, dishStatus)
     local tower = GetTowerFromId(towerId)
     if not tower then return end
-    local dish = tower.Dishes[dishIndex]
-    NetworkExplodeVehicle(dish, false, false)
-    DecorSetInt(dish, "sonrad_dish", 0)
-    -- play a power-down sound for the player
-    local coords = GetEntityCoords(dish)
-    PlaySoundFromCoord(-1, "Power_Down", coords, "DLC_HEIST_HACKING_SNAKE_SOUNDS", 0, 80)
+    tower.DishStatus = dishStatus
+    SyncDishStatus(tower, true)
 end)
-
-RegisterNetEvent("RadioTower:RepairTower")
-AddEventHandler("RadioTower:RepairTower", function(towerId)
-    local tower = GetTowerFromId(towerId)
-    if not tower then return end
-    CreateTowerDishes(tower)
-    for _, e in ipairs(tower.Dishes) do
-        local coords = GetEntityCoords(e)
-        PlaySoundFromCoord(-1, "Success", coords, "DLC_HEIST_HACKING_SNAKE_SOUNDS", 0, 80)
-    end
-end)
-
-local function SetRadioQuality(quality)
-    SendNUIMessage({
-        type = 'set_gamestate',
-        state = { tower_quality = quality }
-    })
-end
 
 CreateThread(function()
     while not NetworkIsPlayerActive(PlayerId()) do
         Wait(10)
     end
     TriggerServerEvent("RadioTower:clientTowerSync")
-end)
+    while #Towers == 0 do Wait(50) end
 
-CreateThread(function()
     DecorRegister("sonrad_dish", 3)
-    while not HasSpawnedTowers do
-        Wait(10)
-    end
     while true do
         local pCoords = GetEntityCoords(GetPlayerPed(-1))
         local quality = 0.0
         for i = 1, #Towers do
             local tower = Towers[i]
-            -- if tower is out of range, then just ignore it
             local d = #(GetTowerCoords(tower) - pCoords)
+            -- if the player is within range (750m), then spawn a physical tower
+            if d < 750.0 and not tower.Spawned and not Config.noPhysicalTowers then
+                CreateTower(tower)
+                DebugPrint(("spawn physical tower (%f) %s"):format(d, tower.Id))
+            elseif d >= 750.0 and tower.Spawned then
+                DestroyTower(tower)
+                DebugPrint(("destroy physical tower (%f) %s"):format(d, tower.Id))
+            end
+
+            -- if tower is out of range, then just ignore it
             if d > tower.Range then goto continue end
 
             local tQuality = (1.0 - (d / tower.Range)) * GetTowerCapacity(tower)
@@ -220,8 +226,11 @@ CreateThread(function()
         else
             DebugPrint(('best tower quality:%.4f'):format(quality))
         end
-        SetRadioQuality(quality)
-        Wait(5000)
+        SendNUIMessage({
+            type = 'set_gamestate',
+            state = { tower_quality = quality }
+        })
+        Wait(3000)
     end
 end)
 
@@ -246,17 +255,26 @@ local function RepairTower(tower)
 
     -- recreate the dishes so they don't accidentally repair the tower twice
     -- waiting for the event to propogate
-    CreateTowerDishes(tower)
     TriggerServerEvent('RadioTower:RepairTower', tower.Id)
 end
 
 CreateThread(function()
-    while not HasSpawnedTowers do
-        Wait(10)
-    end
     while true do
-        local tower, distance = GetClosestTower()
-        if distance < 2.0 and GetTowerCapacity(tower) < 1.0 then
+        -- get the closest (spawned) tower
+        local coords = GetEntityCoords(GetPlayerPed(-1))
+        local tower, d
+        for i = 1, #Towers do
+            local t = Towers[i]
+            if t.Spawned then
+                local td = #(GetTowerCoords(t) - coords)
+                if d == nil or d < td then
+                    tower = t
+                    d = td
+                end
+            end
+        end
+
+        if tower ~= nil and d < 2.0 and GetTowerCapacity(tower) < 1.0 then
             BeginTextCommandDisplayHelp("STRING")
             AddTextComponentSubstringPlayerName("Press ~INPUT_DETONATE~ to repair this tower.")
             EndTextCommandDisplayHelp(0, false, true, -1)
@@ -274,9 +292,6 @@ CreateThread(function()
 end)
 
 CreateThread(function()
-    while not HasSpawnedTowers do
-        Wait(10)
-    end
     while true do
         for i = 1, #Towers do
             local tower = Towers[i]
@@ -307,16 +322,8 @@ end)
 AddEventHandler('onResourceStop', function(resource)
     if resource ~= GetCurrentResourceName() then return end
     for i = 1, #Towers do
-        local tower = Towers[i]
-        if tower.Handle then
-            DeleteEntity(tower.Handle)
-        end
-        if tower.Ladder then
-            DeleteEntity(tower.Ladder)
-        end
-        local n = tower.Dishes and #tower.Dishes or 0
-        for j = 1, n do
-            DeleteEntity(tower.Dishes[j])
-        end
+        DestroyTower(Towers[i])
     end
+    -- make sure the thread doesn't re-spawn them
+    Towers = {}
 end)
