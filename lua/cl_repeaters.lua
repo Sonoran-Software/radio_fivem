20667df69a331e3812f9ff5271ff3d92bf6cb3ab
Jordan.#2139 <jordansilva213@gmail.com>
2023-11-08 14:32:27 -0500

    feat: add trailer controls to cockpit of vehicle

--- lua/cl_repeaters.lua
+++ lua/cl_repeaters.lua
@@ -31,24 +31,38 @@ Citizen.CreateThread(function()
 			TriggerServerEvent('sonoranscripts::updatepos', NetworkGetNetworkIdFromEntity(GetVehiclePedIsIn(PlayerPedId(), false)), GetEntityCoords(GetVehiclePedIsIn(PlayerPedId(), false)))
 		end
 		local entering = GetVehiclePedIsEntering(PlayerPedId())
-		if entering ~= 0 and isRegisteredVehicle(entering) then
+		if entering ~= 0 and isRegisteredVehicle(entering) and (GetPedInVehicleSeat(entering, -1) == PlayerPedId() or GetPedInVehicleSeat(entering, 0) == PlayerPedId()) then
 			if not DecorIsRegisteredAsType('RepeaterActive', 2) then
 				DecorRegister('RepeaterActive', 2)
 			end
 			if not DecorExistOn(entering, 'RepeaterActive') then
 				DecorSetBool(entering, 'RepeaterActive', false)
 			end
-            if not DecorGetBool(entering, 'RepeaterActive') then
-                ShowNotification('Press ~INPUT_CONTEXT~ to enable the radio repeater')
-            else
-                ShowNotification('Press ~INPUT_CONTEXT~ to disable the radio repeater')
-            end
+			if not DecorGetBool(entering, 'RepeaterActive') then
+				ShowNotification('Press ~INPUT_CONTEXT~ to enable the radio repeater')
+			else
+				ShowNotification('Press ~INPUT_CONTEXT~ to disable the radio repeater')
+			end
+		elseif entering ~= 0 and IsVehicleAttachedToTrailer(entering) and (GetPedInVehicleSeat(entering, -1) == PlayerPedId() or GetPedInVehicleSeat(entering, 0) == PlayerPedId()) then
+			local _, trailer = GetVehicleTrailerVehicle(entering)
+			if trailer ~= 0 and isRegisteredVehicle(trailer) then
+				if not DecorIsRegisteredAsType('RepeaterActive', 2) then
+					DecorRegister('RepeaterActive', 2)
+				end
+				if not DecorExistOn(trailer, 'RepeaterActive') then
+					DecorSetBool(trailer, 'RepeaterActive', false)
+				end
+				if not DecorGetBool(trailer, 'RepeaterActive') then
+					ShowNotification('Press ~INPUT_CONTEXT~ to enable the radio repeater on the trailer')
+				else
+					ShowNotification('Press ~INPUT_CONTEXT~ to disable the radio repeater on the trailer')
+				end
+			end
 		end
-        if IsControlJustReleased(0, 38) and entering ~= 0 and isRegisteredVehicle(entering) then
-            DecorSetBool(entering, 'RepeaterActive', not DecorGetBool(entering, 'RepeaterActive'))
-            TriggerServerEvent('sonoranscripts::togglerepeater', NetworkGetNetworkIdFromEntity(entering), DecorGetBool(entering, 'RepeaterActive'),
-                               GetEntityCoords(entering))
+		if IsControlJustReleased(0, 38) and entering ~= 0 and isRegisteredVehicle(entering) then
+			DecorSetBool(entering, 'RepeaterActive', not DecorGetBool(entering, 'RepeaterActive'))
+			TriggerServerEvent('sonoranscripts::togglerepeater', NetworkGetNetworkIdFromEntity(entering), DecorGetBool(entering, 'RepeaterActive'), GetEntityCoords(entering))
 			ShowNotification('Radio repeater ' .. (DecorGetBool(entering, 'RepeaterActive') and 'enabled' or 'disabled'))
-        end
+		end
 	end
 end)
