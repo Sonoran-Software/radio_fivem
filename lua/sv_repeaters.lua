ce6673eaa0a144d3fad79215d9f0f8f34e45f1c0
Jordan.#2139 <jordansilva213@gmail.com>
2023-11-27 14:28:49 -0500

    Update repeater functionality

--- lua/sv_repeaters.lua
+++ lua/sv_repeaters.lua
@@ -4,6 +4,7 @@ local lastLogs = {}
 RegisterNetEvent('sonoranscripts::togglerepeater', function(id, repeater, pos, range)
 	if repeater then
 		if repeaters[id] ~= nil then
+			RegPrint('Updating tower ' .. id)
 			exports['sonoranradio']:updateTower(repeaters[id], nil)
 			repeaters[id] = exports['sonoranradio']:createTower({
 				Destruction = false,
@@ -21,6 +22,7 @@ RegisterNetEvent('sonoranscripts::togglerepeater', function(id, repeater, pos, r
 				DontSaveMe = true
 			})
 		else
+			DebugPrint('Creating tower ' .. id)
 			repeaters[id] = exports['sonoranradio']:createTower({
 				Destruction = false,
 				NotPhysical = true,
@@ -38,26 +40,33 @@ RegisterNetEvent('sonoranscripts::togglerepeater', function(id, repeater, pos, r
 			})
 		end
 	else
+		DebugPrint('Removing tower ' .. id)
 		exports['sonoranradio']:updateTower(repeaters[id], nil)
 		repeaters[id] = nil
 	end
 end)
 
 RegisterNetEvent('sonoranscripts::updatepos', function(id, pos, range)
-	exports['sonoranradio']:updateTower(repeaters[id], {
-		Destruction = false,
-		NotPhysical = true,
-		Swankiness = 0.0,
-		PropPosition = pos,
-		DishStatus = {
-			'alive',
-			'alive',
-			'alive',
-			'alive'
-		},
-		Range = range,
-		Powered = true,
-		DontSaveMe = true
-	})
+	RegPrint('Updating tower ' .. id)
+	if repeaters[id] then
+		exports['sonoranradio']:updateTower(repeaters[id], {
+			Destruction = false,
+			NotPhysical = true,
+			Swankiness = 0.0,
+			PropPosition = pos,
+			DishStatus = {
+				'alive',
+				'alive',
+				'alive',
+				'alive'
+			},
+			Range = range,
+			Powered = true,
+			DontSaveMe = true
+		})
+	end
 end)
 
+function RegPrint(...)
+	print('^5[SONRAD]^7', ...)
+end
