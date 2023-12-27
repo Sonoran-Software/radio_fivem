7d7dce89471308766df037d1fcb86a1b0965b5fe
Jordan.#2139 <65438497+Jordan2139@users.noreply.github.com>
2023-12-27 15:59:12 -0500

    Add chat message for radio skin change event

--- lua/cl_changeFrames.lua
+++ lua/cl_changeFrames.lua
@@ -1,4 +1,10 @@
 RegisterNetEvent('SonoranRadio::AdminSkinChange', function(frame)
+    TriggerEvent('chat:addMessage', {
+        args = {
+            '^1SonoranRadio',
+            'Change your radio skin to ' .. frame .. ''
+        }
+    })
     TriggerServerEvent('SonoranRadio::AdminSkinChange', frame)
     SendNUIMessage({
         type = 'setCurrentSkin',
