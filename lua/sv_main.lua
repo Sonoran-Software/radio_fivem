c895dcdafb617962fe1e53cf68db2fae7d23de5b
Jordan.#2139 <65438497+Jordan2139@users.noreply.github.com>
2023-12-30 17:07:00 -0500

    Add error handling for missing or outdated config file

--- lua/sv_main.lua
+++ lua/sv_main.lua
@@ -179,3 +179,16 @@ TriggerClientEvent('chat:addSuggestion', '/adminskinchange', 'Change your radio 
 		help = 'The frame name to change to'
 	}
 })
+
+
+AddEventHandler('onResourceStart', function(resourceName)
+	if (GetCurrentResourceName() ~= resourceName) then
+		return
+	end
+	if Config.frames == nil or Config.frames.departments == nil or not Config.frames then
+		print('!!! CRITICAL ERROR !!!')
+		print('Config file not found or is outdated. Please ensure your config contains the Config.frames section, did you forget to rename it?')
+		print('!!! CRITICAL ERROR !!!')
+		return
+	end
+end)
