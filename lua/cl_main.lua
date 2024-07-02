d96eb1052e3a6b01365f2e183e6a6342e761f5bb
Jordan.#2139 <65438497+Jordan2139@users.noreply.github.com>
2024-07-02 14:29:30 -0400

    unchange variables, issue seems to not stem from them

--- lua/cl_main.lua
+++ lua/cl_main.lua
@@ -411,9 +411,9 @@ RegisterKeyMapping('+sonradptt', 'Radio PTT', 'keyboard', '|')
 
 function Radio:Talking(toggle)
 	local inVeh = IsPedInAnyVehicle(GetPlayerPed(-1), false)
-	if Radio.TalkAnim then
+	if self.TalkAnim then
 		if toggle and not inVeh then
-			if Radio.Open then
+			if self.Open then
 				RequestAnimDict('cellphone@')
 				while not HasAnimDictLoaded('cellphone@') do
 					Wait(5)
@@ -436,7 +436,7 @@ function Radio:Talking(toggle)
 				isTalking = true
 			end
 		else
-			if Radio.Open then
+			if self.Open then
 				-- cellphone@cellphone_call_to_text
 				-- cellphone@cellphone_text_read_base
 				--
