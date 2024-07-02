5da7da9ad1dfd371aa0baf10d513f29c4054e39a
Jordan.#2139 <jordansilva213@gmail.com>
2024-07-02 17:08:57 -0400

    chore: Update Community ID comment in config file

--- build/sonoranradio/config.CHANGEME.lua
+++ build/sonoranradio/config.CHANGEME.lua
@@ -1,6 +1,6 @@
 Config = {}
 
-Config.comId = 'YOUR COMMUNITY ID' -- IMPORTANT set your API ID here! https://info.sonoranradio.com/standalone-alpha/getting-started/installing-the-in-game-resource
+Config.comId = 'YOUR COMMUNITY ID' -- IMPORTANT set your Community ID here! https://info.sonoranradio.com/standalone-alpha/getting-started/installing-the-in-game-resource
 Config.apiKey = 'YOUR API KEY' -- IMPORTANT set your API Key here! https://info.sonoranradio.com/standalone-alpha/getting-started/installing-the-in-game-resource
 Config.debug = false
 Config.allowUpdateWithPlayers = true
