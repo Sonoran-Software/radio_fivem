baac3a2fa637bf289cdda9fa0f345b31bf346744
Jordan.#2139 <65438497+Jordan2139@users.noreply.github.com>
2024-07-18 01:21:45 -0400

    fix iterator

--- lua/menu/cl_spawner.lua
+++ lua/menu/cl_spawner.lua
@@ -600,21 +600,21 @@ function deletingRadioRepeater()
 			end
 			for k, repeater in ipairs(Towers) do
 				if repeater.Id == state.repeaterId then
-					if DoesEntityExist(k.Ladder) then
-						DeleteEntity(k.Ladder)
+					if DoesEntityExist(repeater.Ladder) then
+						DeleteEntity(repeater.Ladder)
 					end
-					local n = k.Dishes and #k.Dishes or 0
+					local n = repeater.Dishes and #repeater.Dishes or 0
 					for j = 1, n do
-						DeleteEntity(k.Dishes[j])
+						DeleteEntity(repeater.Dishes[j])
 					end
 					table.remove(Towers, k)
 				end
 			end
 			for k, repeater in ipairs(racks) do
 				if repeater.Id == state.repeaterId then
-					local n = k.Servers and #rkack.Servers or 0
+					local n = repeater.Servers and #repeater.Servers or 0
 					for j = 1, n do
-						DeleteEntity(k.Servers[j])
+						DeleteEntity(repeater.Servers[j])
 					end
 					table.remove(racks, k)
 				end
