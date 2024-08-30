e72165f383c827b54d854f9fad35bac89cda50f8
Jordan.#2139 <jordansilva213@gmail.com>
2024-08-30 12:45:10 -0400

    Radio frames fix

--- lua/cl_main.lua
+++ lua/cl_main.lua
@@ -911,6 +911,18 @@ RegisterNetEvent('SonoranRadio::AdminSkinChange', function(frame)
 			type = 'setCurrentSkin',
 			skin = frame
 		})
+	else
+		TriggerEvent('chat:addMessage', {
+			args = {
+				'^1SonoranRadio',
+				'Changed your radio skin to ' .. frame .. ''
+			}
+		})
+		TriggerServerEvent('SonoranRadio::AdminSkinChange_s', frame)
+		SendNUIMessage({
+			type = 'setCurrentSkin',
+			skin = frame
+		})
 	end
 end)
 
