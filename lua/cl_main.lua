7ca9da3fb125e844f1301c31bd40522bd4141e83
Jordan.#2139 <jordansilva213@gmail.com>
2024-09-11 22:12:32 -0400

    feat: Add chat suggestions for commands

--- lua/cl_main.lua
+++ lua/cl_main.lua
@@ -342,7 +342,8 @@ end)
 
 RegisterCommand('radio', radioToggle)
 RegisterCommand('sonradradio', radioToggle)
-
+TriggerEvent('chat:addSuggestion', '/radio', 'Open the Sonoran Radio Interface')
+TriggerEvent('chat:addSuggestion', '/sonradradio', 'Open the Sonoran Radio Interface')
 RegisterCommand('radiotalk', function()
 	Radio.TalkAnim = not Radio.TalkAnim
 	if Radio.TalkAnim then
@@ -351,6 +352,7 @@ RegisterCommand('radiotalk', function()
 		SendNotification('Radio Talk Animation: ~r~Off~r~')
 	end
 end)
+RegisterKeyMapping('radiotalk', 'Toggle Radio Talk Animation', 'keyboard', '')
 
 RegisterCommand('radioreset', function(source, args)
 	SendNUIMessage({
@@ -364,6 +366,7 @@ RegisterCommand('radioreset', function(source, args)
 		})
 	end
 end)
+TriggerEvent('chat:addSuggestion', '/radioreset', 'Reconnect radio to teamspeak', {{name = 'ui', help = 'Reset UI Positions'}})
 
 RegisterCommand('radiohud', function(source, args, rawCommand)
 	-- toggle Radio.Hud
@@ -373,6 +376,7 @@ RegisterCommand('radiohud', function(source, args, rawCommand)
 		size = Radio.Hud
 	})
 end)
+TriggerEvent('chat:addSuggestion', '/radiohud', 'Toggle the radio HUD', {})
 
 RegisterCommand('radiovolume', function(source, args)
 	local volume = tonumber(args[1])
@@ -388,7 +392,7 @@ RegisterCommand('radiovolume', function(source, args)
 	})
 	SendNotification('Radio Volume: ~g~' .. volume .. '%~g~')
 end)
-
+TriggerEvent('chat:addSuggestion', '/radiovolume', 'Change the voice volume of all radios', {{name = 'volume', help = 'The volume percentage (0-250%)'}})
 -- Talking Animation
 -- RegisterCommand('sonradtalk', function()
 -- 	Radio:Talking(isTalking)
