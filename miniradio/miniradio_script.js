b4078664fdaca02e89303cc8ac50d01b91ff3174
Jordan.#2139 <jordansilva213@gmail.com>
2024-09-11 22:08:09 -0400

    feat: tooltip on hover

--- miniradio/miniradio_script.js
+++ miniradio/miniradio_script.js
@@ -32,6 +32,7 @@ function createChannelContent(channelTitle, users) {
 	// Loop through each user and create a span for each, stacked vertically
 	users.forEach((user) => {
 		const userItem = document.createElement("span");
+		userItem.classList.add("userItem"); // Add class for tooltip and styling
 		userItem.textContent = `• ${truncateString(user.name)}`;
 		userItem.title = user.name;
 		userItem.style.fontSize = "12px";
@@ -135,55 +136,54 @@ $(function () {
 	document.onkeyup = function (data) {
 		sendToParent({ type: "keyup", key: data.which, code: data.code });
 	};
-	dragElement(document.getElementById("hudDiv"));
+	dragElement(document.getElementById("hudDiv"), "hudHeader");
 });
 
 function sendToParent(data) {
 	if (parent) parent.postMessage(data, "*");
 }
 
-function dragElement(elmnt) {
-	var pos1 = 0,
-		pos2 = 0,
-		pos3 = 0,
-		pos4 = 0;
-	if (document.getElementById(elmnt.id + "header")) {
-		// if present, the header is where you move the DIV from:
-		document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-	} else {
-		// otherwise, move the DIV from anywhere inside the DIV:
-		elmnt.onmousedown = dragMouseDown;
-	}
+function dragElement(elmnt, dragHandleId) {
+    var pos1 = 0,
+        pos2 = 0,
+        pos3 = 0,
+        pos4 = 0;
 
-	function dragMouseDown(e) {
-		e = e || window.event;
-		e.preventDefault();
-		// get the mouse cursor position at startup:
-		pos3 = e.clientX;
-		pos4 = e.clientY;
-		document.onmouseup = closeDragElement;
-		// call a function whenever the cursor moves:
-		document.onmousemove = elementDrag;
-	}
+    const dragHandle = document.getElementById(dragHandleId);
 
-	function elementDrag(e) {
-		e = e || window.event;
-		e.preventDefault();
-		// calculate the new cursor position:
-		pos1 = pos3 - e.clientX;
-		pos2 = pos4 - e.clientY;
-		pos3 = e.clientX;
-		pos4 = e.clientY;
-		// set the element's new position:
-		elmnt.style.top = elmnt.offsetTop - pos2 + "px";
-		elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
-	}
+    if (dragHandle) {
+        dragHandle.onmousedown = dragMouseDown;
+    }
 
-	function closeDragElement() {
-		// stop moving when mouse button is released:
-		document.onmouseup = null;
-		document.onmousemove = null;
-	}
+    function dragMouseDown(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // get the mouse cursor position at startup:
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        // call a function whenever the cursor moves:
+        document.onmousemove = elementDrag;
+    }
+
+    function elementDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // calculate the new cursor position:
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        // set the element's new position:
+        elmnt.style.top = elmnt.offsetTop - pos2 + "px";
+        elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
+    }
+
+    function closeDragElement() {
+        // stop moving when mouse button is released:
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
 }
 
 window.addEventListener("message", function (event) {
