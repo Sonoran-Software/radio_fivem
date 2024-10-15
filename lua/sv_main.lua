ef514ca5399698bf64d9092489d3d0f459011923
Jordan.#2139 <jordansilva213@gmail.com>
2024-10-15 19:25:37 -0400

    tweak: If unexpected arg throw help message

--- lua/sv_main.lua
+++ lua/sv_main.lua
@@ -638,5 +638,7 @@ SonoranRadio Help
     elseif args[1] == "debugmode" then
         Config.debug = not Config.debug
         infoLog(("Debug mode toggled to %s"):format(tostring(Config.debug)))
+	else
+		print("Missing command. Try \"sonoranradio help\" for help.")
 	end
 end)
