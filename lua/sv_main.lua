15ba6890887abdb59e4f741cbca5ad8e45ffa9a5
Jordan.#2139 <65438497+Jordan2139@users.noreply.github.com>
2024-12-24 16:56:28 -0500

    tweak: Update overwrite log

--- lua/sv_main.lua
+++ lua/sv_main.lua
@@ -514,7 +514,9 @@ AddEventHandler('onResourceStart', function(resourceName)
 	-- Save updated earpieces.json if changes were made
 	if updated then
 		SaveResourceFile(resourceName, 'earpieces.json', json.encode(luaConfig, { indent = true }), -1)
-		infoLog('Updated earpieces.json with missing chatter exclusions.')
+		infoLog('Overwritting earpieces.json with Config.chatterExclusions. Config.chatterExclusions has been depreciated. Please remove this from your config.lua file to prevent any future overwrites')
+		infoLog('Overwritting earpieces.json with Config.chatterExclusions. Config.chatterExclusions has been depreciated. Please remove this from your config.lua file to prevent any future overwrites')
+		infoLog('Overwritting earpieces.json with Config.chatterExclusions. Config.chatterExclusions has been depreciated. Please remove this from your config.lua file to prevent any future overwrites')
 	end
 	chatterConfig = luaConfig
 	if Config.chatterExclusion then
