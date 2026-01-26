077be1e692b0f3ae0e6d45fdfa7275aaafb2ea13
Jacob Parker <blocckba5her@gmail.com>
2026-01-26 13:13:24 -0800

    feat: restore radio on resource restart

--- lua/cl_main.lua
+++ lua/cl_main.lua
@@ -383,6 +383,26 @@ function initClient()
 		return playerHasItem(itemName)
 	end
 
+	function setRadioVisible(visible, frame)
+		if frame then
+			SendNUIMessage({
+				type = 'setCurrentSkin',
+				skin = frame,
+				skins = allowedFrames
+			})
+		end
+		local uiPositions = json.decode(GetResourceKvpString('ui_pos_dic') or '{}')
+		setmetatable(uiPositions, {__jsontype = 'object'})
+		SendNUIMessage({
+			type = 'setUiPositions',
+			data = uiPositions,
+		})
+		SendNUIMessage({
+			type = 'setVisible',
+			visibility = visible,
+			pttKey = getPttKey()
+		})
+	end
 	function radioToggle(frame)
 		TriggerServerEvent('SonoranRadio::CheckPermissions')
 		if not authorized then
@@ -408,24 +428,7 @@ function initClient()
 		end
 
 		radActive = not radActive
-		if frame then
-			SendNUIMessage({
-				type = 'setCurrentSkin',
-				skin = frame,
-				skins = allowedFrames
-			})
-		end
-		local uiPositions = json.decode(GetResourceKvpString('ui_pos_dic') or '{}')
-		setmetatable(uiPositions, {__jsontype = 'object'})
-		SendNUIMessage({
-			type = 'setUiPositions',
-			data = uiPositions,
-		})
-		SendNUIMessage({
-			type = 'setVisible',
-			visibility = radActive,
-			pttKey = getPttKey()
-		})
+		setRadioVisible(radActive, frame)
 		if radActive then
 			SetNuiFocus(true, true)
 		else
@@ -471,6 +474,15 @@ function initClient()
 			skin = frame,
 			skins = allowedFrames
 		})
+
+		if not Radio.Restored and LocalPlayer.state['sonoranradio_restore'] == true then
+			setRadioVisible(true)
+			SendNUIMessage({
+				type = 'pushButton',
+				button = 'power'
+			})
+			Radio.Restored = true
+		end
 	end)
 
 	RegisterCommand('radio', function(_, args)
@@ -1128,6 +1140,7 @@ function initClient()
 		if data.type == 'power' then
 			handleRadioPower(data.power)
 			Radio.On = data.power
+			LocalPlayer.state:set('sonoranradio_restore', data.power, false)
 		end
 
 		if data.type == 'radioConnected' and data.config.myself and not calledSyncAcePerms then
